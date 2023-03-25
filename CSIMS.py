f4d61cfdab11f5fe09477048b50f0d2c7d812850
MAVIS GAMING CENTER <minhltt.bi12-290@st.usth.edu.vn>
2023-03-25 17:25:00 +0700

    fixing code

--- CSIMS.py
+++ CSIMS.py
@@ -1,12 +1,8 @@
 # Create store information management system
 
 # create class Vetements(Clothes)
-class Vetements:
-    def _print(self):
-        pass
 
-# create class Shirt and shirt list
-class Shirt(Vetements):
+class Vetement():
     def __init__(self, name, price, quantity, size, gender):
         self.__name = name
         self.__price = price
@@ -15,215 +11,140 @@ class Shirt(Vetements):
         self.__gender = gender
 
     @property
-    def name(self):
+    def _getName(self):
         return self.__name
 
     @property
-    def price(self):
+    def _getPrice(self):
         return self.__price
-
-    @property
-    def quantity(self):
-        return self.__quantity
-
-    @property
-    def size(self):
-        return self.__size
-
-    @property
-    def gender(self):
-        return self.__gender
     
-    def __str__(self):
-        return f"Name: {self.__name}\t Price: {self.__price}\t Quantity: {self.__quantity}\t Size: {self.__size}\t Gender: {self.__gender}"
-    
-            
-
-shirts = []
-
-# create class Pants and pants list
-class Pants(Vetements):
-    def __init__(self, name, price, quantity, size, gender):
-        self.__name = name
+    @property
+    def _setPrice(self,price):
         self.__price = price
-        self.__quantity = quantity
-        self.__size = size
-        self.__gender = gender
 
     @property
-    def name(self):
-        return self.__name
-
-    @property
-    def price(self):
-        return self.__price
-
-    @property
-    def quantity(self):
+    def _getQuantity(self):
         return self.__quantity
 
     @property
-    def size(self):
+    def _getSize(self):
         return self.__size
 
     @property
-    def gender(self):
+    def _getGender(self):
         return self.__gender
     
-    @property
     def __str__(self):
         return f"Name: {self.__name}\t Price: {self.__price}\t Quantity: {self.__quantity}\t Size: {self.__size}\t Gender: {self.__gender}"
 
-pants = []
 
-# create class Shoes and shoes list
-class Shoes(Vetements):
-    def __init__(self, name, price, quantity, size, gender):
-        self.__name = name
-        self.__price = price
-        self.__quantity = quantity
-        self.__size = size
-        self.__gender = gender
+vetements = []
 
-    @property
-    def name(self):
-        return self.__name
-
-    @property
-    def price(self):
-        return self.__price
-
-    @property
-    def quantity(self):
-        return self.__quantity
-
-    @property
-    def size(self):
-        return self.__size
-
-    @property
-    def gender(self):
-        return self.__gender
+#Create class Employee
+class Employee():
+    def __init__(self,Fname,Lname,id, position, workday, salary):
+        self.__Fname = Fname
+        self.__Lname = Lname
+        self.__id = id
+        self.__position = position
+        self.__workday = workday
+        self.__salary = salary
+    
+    def __str__(self):
+        return f"First name: {self.__Fname}\t Last name: {self.__Lname}\t ID: {self.__id}\t Position: {self.__position}\t Workday: {self.__workday}\t Salary: {self.__salary}"
     
     @property
-    def __str__(self):
-        return f"Name: {self.__name}\t Price: {self.__price}\t Quantity: {self.__quantity}\t Size: {self.__size}\t Gender: {self.__gender}"
-
-shoes = []
-
-# create class dress and dress list
-class Dress(Vetements):
-    def __init__(self, name, price, quantity, size, gender):
-        self.__name = name
-        self.__price = price
-        self.__quantity = quantity
-        self.__size = size
-        self.__gender = gender
-
-    @property
-    def name(self):
-        return self.__name
-
-    @property
-    def price(self):
-        return self.__price
-
-    @property
-    def quantity(self):
-        return self.__quantity
-
-    @property
-    def size(self):
-        return self.__size
-
-    @property
-    def gender(self):
-        return self.__gender
+    def Fname(self):
+        return self.__Fname
     
     @property
-    def __str__(self):
-        return f"Name: {self.__name}\t Price: {self.__price}\t Quantity: {self.__quantity}\t Size: {self.__size}\t Gender: {self.__gender}"
-dress = []
-
-
-# create class skirt and skirt list
-class Skirt(Vetements):
-    def __init__(self, name, price, quantity, size, gender):
-        self.__name = name
-        self.__price = price
-        self.__quantity = quantity
-        self.__size = size
-        self.__gender = gender
+    def Lname(self):
+        return self.__Lname
 
     @property
-    def name(self):
-        return self.__name
+    def id(self):
+        return self.__id
 
     @property
-    def price(self):
-        return self.__price
+    def position(self):
+        return self.__position
 
     @property
-    def quantity(self):
-        return self.__quantity
+    def workday(self):
+        return self.__workday
 
     @property
-    def size(self):
-        return self.__size
+    def salary(self):
+        return self.__salary
+
+employees = []
+
+
+
+
+class Receipt():
+    def __init__(self, customer, vetements,bill):
+        self.__customer = customer
+        self.__vetements = vetements
+        self.__bill = bill
 
     @property
-    def gender(self):
-        return self.__gender
+    def customer(self):
+        return self.__customer
     
     @property
-    def __str__(self):
-        return f"Name: {self.__name}\t Price: {self.__price}\t Quantity: {self.__quantity}\t Size: {self.__size}\t Gender: {self.__gender}"
-skirt = []
-
-# create class underwear and underwear list
-class UnderWear(Vetements):
-    def __init__(self, name, price, quantity, size, gender):
-        self.__name = name
-        self.__price = price
-        self.__quantity = quantity
-        self.__size = size
-        self.__gender = gender
-
-    @property
-    def name(self):
-        return self.__name
-
-    @property
-    def price(self):
-        return self.__price
-
-    @property
-    def quantity(self):
-        return self.__quantity
-
-    @property
-    def size(self):
-        return self.__size
-
-    @property
-    def gender(self):
-        return self.__gender
+    def vetements(self):
+        return self.__vetements
     
     @property
-    def __str__(self):
-        return f"Name: {self.__name}\t Price: {self.__price}\t Quantity: {self.__quantity}\t Size: {self.__size}\t Gender: {self.__gender}"
-underwear = []
+    def bill(self):
+        return self.__bill
 
-for i in range(1,2,1):
-    name = input("Enter name: ")
-    price = int(input("Enter price: "))
-    quantity = int(input("Enter quantity: "))
-    size = input("Enter size: ")
-    gender = input("Enter gender:")
-    shirts.append(Shirt(name,price,quantity,size,gender))
 
-for shirt in shirts:
-    print(shirt)
+
+def inputVetement():
+    number = int(input("Enter number of clothes: "))
+    for i in range(number):
+        name = input("Enter name: ")
+        price = int(input("Enter price: "))
+        quantity = int(input("Enter quantity: "))
+        size = input("Enter size: ")
+        gender = input("Enter gender:")
+        vetements.append(Vetement(name,price,quantity,size,gender))
+
+def showVetement():
+    for i in vetements:
+        print(i)
+
+
+def inputEmployee():
+    number = int(input("Enter number of employee: "))
+    for i in range(number):
+        Fname = input("Enter first name: ")
+        Lname = input("Enter last name: ")
+        id = input("Enter id: ")
+        position = input("Enter position: ")
+        workday = int(input("Enter workday: "))
+        salary = int(input("Enter salary: "))
+        employees.append(Employee(Fname,Lname,id,position,workday,salary))
+
+def CountTotal():
+    while True:
+        showVetement()
+        choice = input("Enter your choice: ")
+        for vetement in vetements:
+            if vetement._name == choice:
+                print("Enter quantity: ")
+                quantity = int(input())
+                bill = quantity * vetement._getPrice()
+                print("Total: ",bill)
+                break
+            else:
+                print("Invalid choice")
+                break
+
+    
+
 # while True:
 #     print("1. Input information")
 #     print("2. Show information")
